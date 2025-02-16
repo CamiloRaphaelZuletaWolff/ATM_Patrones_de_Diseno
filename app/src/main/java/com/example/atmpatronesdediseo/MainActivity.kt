1ce3da1eecc2e2d280973de6c6ce0e8374ce4ad4
CamiloRaphaelZuletaWolff <camiloraphaelzuletawolff@gmail.com>
2025-02-16 18:41:10 -0400

    Commit Pantalla Retiro

--- app/src/main/java/com/example/atmpatronesdediseo/MainActivity.kt
+++ app/src/main/java/com/example/atmpatronesdediseo/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.atmpatronesdediseo
 
-import android.graphics.Color
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,14 +11,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -30,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,26 +38,23 @@ class MainActivity : ComponentActivity() {
             ATMPatronesDeDiseñoTheme {
                 // Estado para controlar qué pantalla mostrar
                 var mostrarSeleccion by remember { mutableStateOf(false) }
-
-                var saldo by remember { mutableStateOf(0) }
-
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     if (mostrarSeleccion) {
                         // Mostrar la nueva pantalla de selección
                         PantallaSeleccion(
-                            saldo = saldo,
+                            saldo = 0,
                             onDepositarClick = { /* Lógica para depositar */ },
                             onRetirarClick = { /* Lógica para retirar */ },
                             onVolverClick = { mostrarSeleccion = false } // Volver a la pantalla anterior
                         )
                     } else {
                         // Mostrar la pantalla anterior
-                        PantallaInicial(modifier = Modifier.padding(innerPadding)) { innerModifier ->
+                        Pantallas(modifier = Modifier.padding(innerPadding)) { innerModifier ->
                             Column(
                                 modifier = innerModifier,
                                 horizontalAlignment = Alignment.CenterHorizontally
                             ) {
-                                contenidoPantalla()
+                                contenidoPantallaInicial()
                                 Button(
                                     onClick = { mostrarSeleccion = true }, // Ir a la pantalla de selección
                                     modifier = Modifier
@@ -82,7 +73,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PantallaInicial(modifier: Modifier = Modifier, contenido : @Composable  (m: Modifier) -> Unit) {
+fun Pantallas(modifier: Modifier = Modifier, contenido : @Composable  (m: Modifier) -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -102,6 +93,7 @@ fun PantallaInicial(modifier: Modifier = Modifier, contenido : @Composable  (m: 
 
 }
 
+
 @Composable
 fun PantallaSeleccion(saldo : Int,
                       onDepositarClick: () -> Unit,
@@ -109,9 +101,9 @@ fun PantallaSeleccion(saldo : Int,
                       onVolverClick: () -> Unit, // Callback para volver a la pantalla anterior
                       modifier: Modifier = Modifier
 ){
-    PantallaInicial(modifier = modifier) { innerModifier ->
+    Pantallas(modifier = modifier) {
         Column(
-            modifier = innerModifier
+            modifier = modifier
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
@@ -161,22 +153,22 @@ fun PantallaSeleccion(saldo : Int,
 
 
 @Composable
-fun contenidoPantalla(modifier: Modifier = Modifier) {
+fun contenidoPantallaInicial(modifier: Modifier = Modifier) {
     Column(horizontalAlignment = Alignment.CenterHorizontally){
         Column(modifier.padding(top = 50.dp, bottom = 40.dp)){
         TextField(value ="", onValueChange = {}, label = { Text("USUARIO") })
         TextField(value ="", onValueChange = {}, label = { Text("CONTRASEÑA") })
         }
-        Numeros()
+        BotonesNumeros()
         Button(onClick = { /*TODO*/ },content = { Text("Cargar") })             }
 
 }
 
 @Composable
-fun Numeros(modifier: Modifier = Modifier) {
+fun BotonesNumeros(modifier: Modifier = Modifier) {
     LazyVerticalGrid(modifier = Modifier.background(androidx.compose.ui.graphics.Color.Gray), columns = GridCells.Fixed(3)) {
         items(9) {numero ->
-            botonItem(numero = numero+1)
+            BotonItem(numero = numero+1)
 
         }
     }
@@ -184,7 +176,7 @@ fun Numeros(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun botonItem(numero: Int) {
+fun BotonItem(numero: Int) {
     Button(
         onClick = { /*TODO*/ },
         modifier = Modifier
@@ -197,7 +189,76 @@ fun botonItem(numero: Int) {
 
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun contenidoPantallaRetiro(){
+    Column(
+        modifier = Modifier
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Saldo actual: 0",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+
+        TextField("", {}, label = { Text("Monto a retirar")})
+        // Botón para depositar
+        Button(
+            onClick =
+            {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        ) {
+            Text(text = "Retirar", fontSize = 20.sp)
+        }
+
+
+    }
+}
+
+
+
+
+@Composable
+fun contenidoPantallaDepositar(){
+    Column(
+        modifier = Modifier
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Saldo actual: 0",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+
+        TextField("", {}, label = { Text("Monto a retirar")})
+        // Botón para depositar
+        Button(
+            onClick =
+            {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        ) {
+            Text(text = "Retirar", fontSize = 20.sp)
+        }
+
+
+    }
+}
+
+
+
+
+@Preview()
 @Composable
 fun PantallaSeleccionPreview() {
     ATMPatronesDeDiseñoTheme {
@@ -211,29 +272,20 @@ fun PantallaSeleccionPreview() {
     }
 }
 
-@Preview
 @Composable
-private fun NumerosPreview() {
-    Numeros()
-
-
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun PantallaInicial(name: String, modifier: Modifier = Modifier) {
     Column(modifier = Modifier
         .fillMaxSize()
         .background(androidx.compose.ui.graphics.Color.DarkGray)) {
-        PantallaInicial(contenido = {contenidoPantalla()})
-        Numeros()
+        Pantallas(contenido = {contenidoPantallaInicial()})
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PantallaInicialPreview() {
     ATMPatronesDeDiseñoTheme {
-        Greeting("Android")
+        PantallaInicial("Android")
     }
 }
+
